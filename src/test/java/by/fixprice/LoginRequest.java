5887fa39741caf056efd470913f99f7662f9151d
Pavel <pamitrovel@gmail.com>
2024-11-10 13:50:30 +0300

    add LoginRequest LOGIN_URL and getHeaders

---
+++ src/test/java/by/fixprice/LoginRequest.java
@@ -0,0 +1,16 @@
+package by.fixprice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest {
+    public static final String LOGIN_URL = "https://api.fix-price.by/buyer/v2/auth/login";
+
+    public static Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+        headers.put("x-city", "14");
+        headers.put("x-key", "740e56af4c394537d535819f54ba29cc");
+        return headers;
+    }
+}
